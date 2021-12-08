6de48bd2304074b4cce5c8b6354887015cc4e95b
Bruce Nguyen <bruceleenguyen95@gmail.com>
2021-12-08 09:19:13 -0700

    created setup script

--- nvim/plugin/nvim-lspconfig.lua
+++ nvim/plugin/nvim-lspconfig.lua
@@ -68,6 +68,8 @@ cmp.setup.cmdline(':', {
   })
 })
 
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- lua
 local sumneko_binary_path = vim.fn.exepath('lua-language-server')
 local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
---
+++ nvim/setup.sh
@@ -0,0 +1,14 @@
+# vimls
+npm install -g vim-language-server
+
+# bashls
+npm install -g bash-language-server
+
+# tsserver
+npm install -g typescript typescript-language-server
+
+# html
+npm install -g vscode-langservers-extracted
+
+# pylsp
+pipx install 'python-lsp-server[all]'
