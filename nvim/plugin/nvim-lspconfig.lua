68a91c0900ededb609247a6590dce20d7249a369
Bruce Nguyen <bruceleenguyen95@gmail.com>
2021-12-08 09:07:16 -0700

    added lua file

---
+++ nvim/plugin/nvim-lspconfig.lua
@@ -0,0 +1,142 @@
+local function map(mode, combo, mapping, opts)
+  local options = {noremap = true}
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, combo, mapping, options)
+end
+
+map('n', '<leader>d', ':lua vim.lsp.buf.definition()<CR>')
+map('n', '<leader>i' ,':lua vim.lsp.buf.implementation()<CR>')
+map('n', '<leader>sh', ':lua vim.lsp.buf.signature_help()<CR>')
+map('n', '<leader>rr', ':lua vim.lsp.buf.references()<CR>')
+map('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+map('n', '<leader>h', ':lua vim.lsp.buf.hover()<CR>')
+map('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>')
+map('n', '<leader>sd', ':lua vim.lsp.util.show_line_diagnostics(); vim.lsp.util.show_line_diagnostics()<CR>')
+
+-- nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- lua
+local sumneko_binary_path = vim.fn.exepath('lua-language-server')
+local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require('lspconfig').sumneko_lua.setup {
+  cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      }
+    }
+  }
+}
+
+-- c#
+local pid = vim.fn.getpid()
+local omnisharp_bin = "/home/brucen/tools/omnisharp/run"
+require('lspconfig').omnisharp.setup {
+  cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+  capabilities = capabilities
+}
+
+-- typescript
+require('lspconfig').tsserver.setup {
+  capabilities = capabilities
+}
+
+-- html
+local htmlCapabilities = vim.lsp.protocol.make_client_capabilities()
+htmlCapabilities.textDocument.completion.completionItem.snippetSupport = true
+require('lspconfig').html.setup {
+  capabilities = htmlCapabilities
+}
+
+-- python
+require('lspconfig').pylsp.setup {
+  capabilities = capabilities
+}
+-- require('lspconfig').pyright.setup{}
+
+-- bash
+require('lspconfig').bashls.setup {
+  capabilities = capabilities
+}
+
+-- vim
+require('lspconfig').vimls.setup {
+  capabilities = capabilities
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- Disable signs
+    signs = false,
+  }
+)
