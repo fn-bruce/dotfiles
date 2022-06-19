d703dbc72df2b30dcde7636e85f831893c8e2fed
Bruce Nguyen <bruceleenguyen95@gmail.com>
2022-06-19 10:41:23 -0600

    update

--- nvim/lua/user/dap/configurations/cs.lua
+++ nvim/lua/user/dap/configurations/cs.lua
@@ -1,10 +1,21 @@
 return {
-	{
-		type = "coreclr",
-		name = "launch - netcoredbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-		end,
-	},
-}
+    {
+      type = "coreclr",
+      name = "launch - netcoredbg",
+      request = "launch",
+      program = function()
+        local cwd = vim.fn.getcwd()
+        return vim.fn.input('Path to dll: ', cwd .. '/bin/Debug/', 'file')
+      end,
+    },
+    {
+      type = "coreclr",
+      name = "attach - netcoredbg",
+      request = "attach",
+      processId = function()
+        local pid = require('dap.utils').pick_process()
+        vim.fn.setenv('NETCOREDBG_ATTACH_PID', pid)
+        return pid
+      end,
+    },
+  }
