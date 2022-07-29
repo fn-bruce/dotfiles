967faed87a59f505687827c3dd17a48fae3727c0
Bruce Nguyen <bruceleenguyen95@gmail.com>
2022-07-29 10:33:42 -0600

    update

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -66,7 +66,7 @@ cmp.setup({
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<ESC>"] = cmp.mapping({
+		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
