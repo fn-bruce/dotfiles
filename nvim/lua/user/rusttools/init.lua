0409a8798ec90c15a399dd9e232a9a375443061c
Bruce Nguyen <bruceleenguyen95@gmail.com>
2022-12-13 22:37:08 -0700

    update

--- nvim/lua/user/rusttools/init.lua
+++ nvim/lua/user/rusttools/init.lua
@@ -5,9 +5,9 @@ function M.setup()
 	local lsp_handlers = require("user.lsp.handlers")
 
 	-- for debugging
-	local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.6.7/"
+	local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.8.1/"
 	local codelldb_path = extension_path .. "adapter/codelldb"
-	local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+	local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 	rt.setup({
 		server = {
