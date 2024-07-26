c11922383bb835b9e481f2c28662e217d3aa90dd
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-07-26 14:37:19 -0600

    update

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s i3/config ~/.config/i3/config
+ln -s i3status/config ~/.config/i3status/config
+ln -s nvim ~/.config/nvim
+ln -s kitty ~/.config/kitty
+ln -s tmux/.tmux.conf ~/.tmux.conf
+ln -s zsh/.zshrc ~/.zshrc
