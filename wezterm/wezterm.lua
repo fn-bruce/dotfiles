77bc90e4e30c2a010f959038a7826a2aa023b782
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-06-14 02:16:00 -0600

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,5 +15,15 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 20
+
+config.window_padding = {
+	top = 10,
+	right = 0,
+	left = 10,
+	bottom = 0,
+}
+
 -- and finally, return the configuration to wezterm
 return config
