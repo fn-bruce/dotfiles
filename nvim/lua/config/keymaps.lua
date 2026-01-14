8bba70dab4b320d20aae982da9cb08746218c942
Bruce Nguyen <bruceleenguyen95@gmail.com>
2026-01-14 08:32:15 -0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+local wk = require("which-key")
+wk.add({
+  { "<leader>l", group = "lazy/leetcode" },
+  { "<leader>lc", group = "leetcode" },
+})
--- nvim/lua/plugins/leetcode.lua
+++ nvim/lua/plugins/leetcode.lua
@@ -9,4 +9,17 @@ return {
   opts = {
     -- configuration goes here
   },
+  keys = {
+    { "<leader>lcq", "<cmd>Leet<cr>", desc = "Open Leetcode Menu" },
+    { "<leader>lcl", "<cmd>Leet list<cr>", desc = "List Problems" },
+    { "<leader>lcd", "<cmd>Leet desc<cr>", desc = "Toggle Description" },
+    { "<leader>lcr", "<cmd>Leet run<cr>", desc = "Run Code" },
+    { "<leader>lcs", "<cmd>Leet submit<cr>", desc = "Submit Solution" },
+    { "<leader>lci", "<cmd>Leet info<cr>", desc = "Problem Info" },
+    { "<leader>lcc", "<cmd>Leet console<cr>", desc = "Toggle Console" },
+    { "<leader>lct", "<cmd>Leet tabs<cr>", desc = "Show Tabs" },
+    { "<leader>lcy", "<cmd>Leet yank<cr>", desc = "Yank Solution" },
+    { "<leader>lcn", "<cmd>Leet random<cr>", desc = "Random Problem" },
+    { "<leader>lco", "<cmd>Leet open<cr>", desc = "Open in Browser" },
+  }
 }
