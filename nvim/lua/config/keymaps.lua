c46fae81c05f6ddbb11cbd51dd85a40fdf29d1e3
Bruce Nguyen <bruceleenguyen95@gmail.com>
2026-01-15 20:18:30 -0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,13 +7,6 @@ vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Diagnosti
 -- exit terminal mode
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'exit terminal mode' })
 
--- keybinds to make split navigation easier.
---  use ctrl+<hjkl> to switch between windows
-vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'move focus to the left window' })
-vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'move focus to the right window' })
-vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'move focus to the lower window' })
-vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'move focus to the upper window' })
-
 -- shortcut for lazy
 vim.keymap.set('n', '<leader>L', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
