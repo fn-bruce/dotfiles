6e6ca43efc0ab7cee5fa4da0a332142663c8d930
Bruce Nguyen <bruceleenguyen95@gmail.com>
2026-01-15 20:41:44 -0700

    update

--- nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,37 +1,18 @@
 return {
-  'alexghergh/nvim-tmux-navigation',
-  config = function()
-    require('nvim-tmux-navigation').setup {
-      disable_when_zoomed = true, -- defaults to false
-      keybindings = {
-        left = '<C-h>',
-        down = '<C-j>',
-        up = '<C-k>',
-        right = '<C-l>',
-        last_active = '<C-\\>',
-        next = '<C-Space>',
-      },
-    }
-
-    local function tmux_command(command)
-      local tmux_socket = vim.fn.split(vim.env.TMUX, ',')[1]
-      return vim.fn.system('tmux -S ' .. tmux_socket .. ' ' .. command)
-    end
-
-    local nvim_tmux_nav_group = vim.api.nvim_create_augroup('NvimTmuxNavigation', {})
-
-    vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResume' }, {
-      group = nvim_tmux_nav_group,
-      callback = function()
-        tmux_command 'set-option -p @is_vim yes'
-      end,
-    })
-
-    vim.api.nvim_create_autocmd({ 'VimLeave', 'VimSuspend' }, {
-      group = nvim_tmux_nav_group,
-      callback = function()
-        tmux_command 'set-option -p -u @is_vim'
-      end,
-    })
-  end,
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+  },
 }
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,8 +2,21 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = '*',
-    config = {
-      open_mapping = [[<c-\>]],
+    keys = {
+      {
+        '<C-/>',
+        '<cmd>ToggleTerm<cr>',
+        mode = { 'n', 't' },
+        desc = 'Toggle terminal',
+      },
+      {
+        '<C-_>',
+        '<cmd>ToggleTerm<cr>',
+        mode = { 'n', 't' },
+        desc = 'Toggle terminal',
+      },
+    },
+    opts = {
       shade_terminals = false,
     },
   },
