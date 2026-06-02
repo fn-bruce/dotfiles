a008e1b80d66ec99606bb64bf576c5367196dd34
Bruce Nguyen <bruceleenguyen95@gmail.com>
2026-06-02 13:53:22 -0600

    update

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,77 +1,61 @@
 return {
   'obsidian-nvim/obsidian.nvim',
   version = '*',
+  ft = 'markdown', -- so opening a note loads the plugin and fires enter_note
+  cmd = 'Obsidian',
+  keys = {
+    -- Entry points: valid from ANY buffer (operate on the default/first workspace)
+    { '<leader>on', '<cmd>Obsidian new<cr>', desc = 'New note' },
+    { '<leader>oN', '<cmd>Obsidian new_from_template<cr>', desc = 'New from template' },
+    { '<leader>ou', '<cmd>Obsidian unique_note<cr>', desc = 'New unique note' },
+    { '<leader>of', '<cmd>Obsidian quick_switch<cr>', desc = 'Find note' },
+    { '<leader>os', '<cmd>Obsidian search<cr>', desc = 'Search notes' },
+    { '<leader>ot', '<cmd>Obsidian tags<cr>', desc = 'Tags' },
+    { '<leader>od', '<cmd>Obsidian today<cr>', desc = 'Today' },
+    { '<leader>oD', '<cmd>Obsidian dailies<cr>', desc = 'Dailies list' },
+    { '<leader>o]', '<cmd>Obsidian tomorrow<cr>', desc = 'Tomorrow' },
+    { '<leader>o[', '<cmd>Obsidian yesterday<cr>', desc = 'Yesterday' },
+    { '<leader>ow', '<cmd>Obsidian workspace<cr>', desc = 'Switch workspace' },
+  },
   opts = {
     callbacks = {
       enter_note = function(_)
-        local map = function(key, cmd, desc)
-          vim.keymap.set('n', key, '<cmd>Obsidian ' .. cmd .. '<cr>', { buffer = true, desc = desc })
+        local map = function(key, cmd, desc, mode)
+          vim.keymap.set(mode or 'n', key, '<cmd>Obsidian ' .. cmd .. '<cr>', { buffer = true, desc = desc })
         end
 
-        -- Creation
-        map('<leader>on', 'new', 'New note')
-        map('<leader>oN', 'new_from_template', 'New from template')
-        map('<leader>ou', 'unique_note', 'New unique note')
+        -- Note-context actions: only meaningful inside the current note
         map('<leader>oi', 'template', 'Insert template')
-
-        -- Navigation / Search
-        map('<leader>of', 'quick_switch', 'Find note')
-        map('<leader>os', 'search', 'Search notes')
         map('<leader>ob', 'backlinks', 'Backlinks')
-        map('<leader>ot', 'tags', 'Tags')
         map('<leader>oT', 'toc', 'Table of contents')
         map('<leader>ol', 'links', 'Links in note')
-
-        -- Daily notes
-        map('<leader>od', 'today', 'Today')
-        map('<leader>oD', 'dailies', 'Dailies list')
-        map('<leader>o]', 'tomorrow', 'Tomorrow')
-        map('<leader>o[', 'yesterday', 'Yesterday')
-
-        -- Note actions
         map('<leader>or', 'rename', 'Rename note')
         map('<leader>op', 'paste_img', 'Paste image')
 
-        -- Workspace
-        map('<leader>ow', 'workspace', 'Switch workspace')
-
-        -- Visual mode
-        vim.keymap.set('v', '<leader>oe', '<cmd>Obsidian extract_note<cr>', { buffer = true, desc = 'Extract note' })
-        vim.keymap.set('v', '<leader>oL', '<cmd>Obsidian link_new<cr>', { buffer = true, desc = 'Link to new note' })
-        vim.keymap.set('v', '<leader>ol', '<cmd>Obsidian link<cr>', { buffer = true, desc = 'Link selection' })
+        -- Visual mode (act on the selection in the current note)
+        map('<leader>oe', 'extract_note', 'Extract note', 'v')
+        map('<leader>oL', 'link_new', 'Link to new note', 'v')
+        map('<leader>ol', 'link', 'Link selection', 'v')
       end,
     },
     legacy_commands = false,
     workspaces = {
-      {
-        name = 'personal',
-        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Personal',
-      },
-      {
-        name = 'work',
-        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Work',
-      },
+      { name = 'personal', path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Personal' },
+      { name = 'work', path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Work' },
     },
     templates = {
-      folder = 'Templates', -- relative to workspace root
-      date_format = '%Y-%m-%d',
+      folder = 'templates',
+      date_format = '%Y-%m-%d-%a',
       time_format = '%H:%M',
+      customizations = {
+        ['daily'] = { notes_subdir = 'daily' },
+        ['fleeting'] = { notes_subdir = 'fleeting' },
+        ['literature'] = { notes_subdir = 'literature' },
+        ['permanent'] = { notes_subdir = 'permanent' },
+        ['reference'] = { notes_subdir = 'reference' },
+      },
     },
-    daily_notes = {
-      folder = 'Daily',
-    },
-    frontmatter = {
-      enabled = true,
-      func = function(note)
-        local out = require('obsidian.builtin').frontmatter(note)
-
-        if not out.created then
-          out.created = os.date '%Y-%m-%dT%H:%M:%S'
-        end
-
-        return out
-      end,
-      sort = { 'id', 'aliases', 'tags', 'created' },
-    },
+    daily_notes = { folder = 'daily' },
+    frontmatter = { enabled = false },
   },
 }
